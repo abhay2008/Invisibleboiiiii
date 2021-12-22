a652e31b345a418251ae3464c68f398149e2da71
Abhay Kashyap <71137700+abhay2008@users.noreply.github.com>
2021-12-22 17:07:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 output = cv2.VideoWriter("output.avi", fourcc, 20.0, (640, 480))
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 time.sleep(2)
 
 bg = 0
@@ -19,10 +19,10 @@ while (cap.isOpened()):
   np.flip(img, axis=1)
   hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
   lower_black = np.array([30, 30, 0])
-  upper_black = np.array([104, 153, 0])
+  upper_black = np.array([40, 69, 0])
   mask1 = cv2.inRange(hsv, lower_black, upper_black)
-  lower_black = np.array([60, 60, 0])
-  upper_black = np.array([145, 178, 0])
+  lower_black = np.array([0, 0, 0])
+  upper_black = np.array([10, 40, 0])
   mask2 = cv2.inRange(hsv, lower_black, upper_black)
   mask1 = mask1 + mask2
   mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
